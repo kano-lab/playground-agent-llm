7fdc29b3d72da306bdeeff83a2c84568f8807695
nharu-0630 <8305330+nharu-0630@users.noreply.github.com>
2025-03-19 17:54:24 +0900

    [add] langchainを追加

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -5,18 +5,22 @@ from __future__ import annotations
 from time import sleep
 from typing import TYPE_CHECKING
 
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
+from pydantic import SecretStr
+
 from utils.timeout import timeout
 
 if TYPE_CHECKING:
     from configparser import ConfigParser
 
-    from google.genai import Client
-    from google.genai.chats import Chat
+    from langchain_core.language_models.chat_models import BaseChatModel
+    from langchain_core.messages import BaseMessage
 
 import random
 
 from aiwolf_nlp_common.packet import Info, Packet, Request, Role, Setting, Status, Talk
-from google import genai
 
 from utils.agent_logger import AgentLogger
 
@@ -44,10 +48,10 @@ class Agent:
         self.idx = idx
         self.role = role
 
-        self.client: Client | None = None
-        self.chat: Chat | None
         self.sent_talk_count: int = 0
         self.sent_whisper_count: int = 0
+        self.llm_model: BaseChatModel | None = None
+        self.llm_message_history: list[BaseMessage] = []
 
     def set_packet(self, packet: Packet) -> None:
         """パケット情報をセットする."""
@@ -63,6 +67,7 @@ class Agent:
         if self.request == Request.INITIALIZE:
             self.talk_history: list[Talk] = []
             self.whisper_history: list[Talk] = []
+            self.llm_message_history: list[BaseMessage] = []
         self.agent_logger.logger.debug(packet)
 
     def get_alive_agents(self) -> list[str]:
@@ -73,6 +78,26 @@ class Agent:
             return []
         return [k for k, v in self.info.status_map.items() if v == Status.ALIVE]
 
+    def _send_message_to_llm(self, content: str) -> str | None:
+        if self.llm_model is None:
+            self.agent_logger.logger.error("LLM is not initialized")
+            return None
+        try:
+            self.llm_message_history.append(HumanMessage(content=content))
+            response = self.llm_model.invoke(self.llm_message_history)
+            response_content = (
+                response.content
+                if isinstance(response.content, str)
+                else str(response.content[0])
+            )
+            self.llm_message_history.append(AIMessage(content=response_content))
+            self.agent_logger.logger.info(["LLM", content, response_content])
+        except Exception:
+            self.agent_logger.logger.exception("Failed to send message to LLM")
+            return None
+        else:
+            return response_content
+
     @timeout
     def name(self) -> str:
         """名前リクエストに対する応答を返す."""
@@ -82,30 +107,65 @@ class Agent:
         """ゲーム開始リクエストに対する初期化処理を行う."""
         if self.config is None or self.info is None:
             return
-        self.client = genai.Client(api_key=self.config.get("gemini", "api_key"))
-        self.chat = self.client.chats.create(model=self.config.get("gemini", "model"))
-        self.chat.send_message(
-            f"""
-            あなたは人狼ゲームのエージェントです。
-            あなたの名前は{self.info.agent}です。
-            あなたの役職は{self.role}です。
 
-            これからゲームを進行していきます。リクエストが来た際には、適切な応答を返してください。
-            トークリクエストと囁きリクエストに対しては、ゲーム内で発言するべき内容のみを出力してください。
-            他のリクエストに対しては、行動の対象となるエージェントの名前のみを出力してください。
+        model_type = self.config.get("llm", "type")
+        match model_type:
+            case "openai":
+                self.llm_model = ChatOpenAI(
+                    model=self.config.get("openai", "model"),
+                    temperature=self.config.getfloat("openai", "temperature"),
+                    api_key=SecretStr(self.config.get("openai", "api_key")),
+                )
+            case "google":
+                self.llm_model = ChatGoogleGenerativeAI(
+                    model=self.config.get("google", "model"),
+                    temperature=self.config.getfloat("google", "temperature"),
+                    api_key=SecretStr(self.config.get("google", "api_key")),
+                )
+            case _:
+                raise ValueError(model_type, "Unknown LLM type")
 
-            説明は以上です。「はい」と出力してください。
-            """,
-        )
+        prompt = f"""
+        あなたは人狼ゲームのエージェントです。
+        あなたの名前は{self.info.agent}です。
+        あなたの役職は{self.role}です。
+
+        これからゲームを進行していきます。リクエストが来た際には、適切な応答を返してください。
+
+        トークリクエストと囁きリクエストに対しては、ゲーム内で発言するべき内容のみを出力してください。
+        履歴がある場合は、それを参考にしてください。ない場合は、適切な内容を出力してください。
+        これ以上の情報を得られないと考えたときなどトークを終了したい場合については「Over」と出力してください。
+
+        他のリクエストに対しては、行動の対象となるエージェントの名前のみを出力してください。
+        対象となる生存しているエージェントの一覧が付与されています。
+
+        あなたのレスポンスはそのままゲーム内に送信されるため、不要な情報を含めないでください。
+        """
+        self._send_message_to_llm(prompt)
 
     def daily_initialize(self) -> None:
         """昼開始リクエストに対する処理を行う."""
+        message = "昼開始リクエスト"
+        if self.info is not None:
+            message += f"{self.info.day}日目\n"
+            if self.info.medium_result is not None:
+                message += f"霊能結果: {self.info.medium_result}\n"
+            if self.info.divine_result is not None:
+                message += f"占い結果: {self.info.divine_result}\n"
+            if self.info.executed_agent is not None:
+                message += f"追放結果: {self.info.executed_agent}\n"
+            if self.info.attacked_agent is not None:
+                message += f"襲撃結果: {self.info.attacked_agent}\n"
+            if self.info.vote_list is not None:
+                message += f"投票結果: {self.info.vote_list}\n"
+            if self.info.attack_vote_list is not None:
+                message += f"襲撃投票結果: {self.info.attack_vote_list}\n"
+        sleep(3)
+        self._send_message_to_llm(message)
 
     @timeout
     def whisper(self) -> str:
         """囁きリクエストに対する応答を返す."""
-        if self.chat is None:
-            return ""
         message = f"""
         囁きリクエスト
         履歴:
@@ -118,13 +178,11 @@ class Agent:
         """
         sleep(3)
         self.sent_whisper_count = len(self.whisper_history)
-        return (self.chat.send_message(message).text or "").strip()
+        return self._send_message_to_llm(message) or ""
 
     @timeout
     def talk(self) -> str:
         """トークリクエストに対する応答を返す."""
-        if self.chat is None:
-            return ""
         message = f"""
         トークリクエスト
         履歴:
@@ -137,68 +195,78 @@ class Agent:
         """
         sleep(3)
         self.sent_talk_count = len(self.talk_history)
-        return (self.chat.send_message(message).text or "").strip()
+        return self._send_message_to_llm(message) or ""
 
     def daily_finish(self) -> None:
         """昼終了リクエストに対する処理を行う."""
+        message = "昼終了リクエスト"
+        if self.info is not None:
+            message += f"{self.info.day}日目\n"
+            if self.info.medium_result is not None:
+                message += f"霊能結果: {self.info.medium_result}\n"
+            if self.info.divine_result is not None:
+                message += f"占い結果: {self.info.divine_result}\n"
+            if self.info.executed_agent is not None:
+                message += f"追放結果: {self.info.executed_agent}\n"
+            if self.info.attacked_agent is not None:
+                message += f"襲撃結果: {self.info.attacked_agent}\n"
+            if self.info.vote_list is not None:
+                message += f"投票結果: {self.info.vote_list}\n"
+            if self.info.attack_vote_list is not None:
+                message += f"襲撃投票結果: {self.info.attack_vote_list}\n"
+        sleep(3)
+        self._send_message_to_llm(message)
 
     @timeout
     def divine(self) -> str:
         """占いリクエストに対する応答を返す."""
-        if self.chat is None:
-            return random.choice(self.get_alive_agents())  # noqa: S311
         message = f"""
         占いリクエスト
         対象:
         {"\n".join(self.get_alive_agents())}
         """
         sleep(3)
-        return self.chat.send_message(message).text or random.choice(  # noqa: S311
+        return self._send_message_to_llm(message) or random.choice(  # noqa: S311
             self.get_alive_agents(),
         )
 
     @timeout
     def guard(self) -> str:
         """護衛リクエストに対する応答を返す."""
-        if self.chat is None:
-            return random.choice(self.get_alive_agents())  # noqa: S311
         message = f"""
         護衛リクエスト
         対象:
         {"\n".join(self.get_alive_agents())}
         """
         sleep(3)
-        return self.chat.send_message(message).text or random.choice(  # noqa: S311
+        return self._send_message_to_llm(message) or random.choice(  # noqa: S311
             self.get_alive_agents(),
         )
 
     @timeout
     def vote(self) -> str:
         """投票リクエストに対する応答を返す."""
-        if self.chat is None:
-            return random.choice(self.get_alive_agents())  # noqa: S311
         message = f"""
         投票リクエスト
         対象:
         {"\n".join(self.get_alive_agents())}
         """
         sleep(3)
-        return self.chat.send_message(message).text or random.choice(  # noqa: S311
+        return self._send_message_to_llm(message) or random.choice(  # noqa: S311
             self.get_alive_agents(),
         )
 
     @timeout
     def attack(self) -> str:
         """襲撃リクエストに対する応答を返す."""
-        if self.chat is None:
-            return random.choice(self.get_alive_agents())  # noqa: S311
         message = f"""
         襲撃リクエスト
         対象:
         {"\n".join(self.get_alive_agents())}
         """
         sleep(3)
-        return self.chat.send_message(message).text or random.choice(  # noqa: S311
+        sleep(3)
+        return self._send_message_to_llm(message) or random.choice(  # noqa: S311
             self.get_alive_agents(),
         )
 
